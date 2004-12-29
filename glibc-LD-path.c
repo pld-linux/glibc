07303b8f4d08e11d75577736e6dd093086d43bbe
cvs2git <feedback@pld-linux.org>
2004-12-29 11:59:07 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.1.2'.
    Cherrypick from master 2004-12-29 11:59:07 UTC Elan Ruusamäe <glen@pld-linux.org> '- helper program to kill interferring glibc lib dirs during glibc upgrade':
        glibc-LD-path.c -> 1.1

---
+++ glibc-LD-path.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2004 Elan Ruusamäe <glen@pld-linux.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	  This product includes software developed by Elan Ruusamäe
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ELAN RUUSAMÄE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Usage
+ * ~~~~~
+ *
+ * This simple program is aimed to be used as a workaround when installing over
+ * glibc which has placed glibc.so into /lib/i686.
+ *
+ * For example, in glibc.spec:
+ *
+ * %post -p /sbin/postshell
+ * /sbin/glibc-postrm /%{_lib}/i686
+ *
+ * Patches and bugreports are welcome, direct them to Elan Ruusamäe
+ * <glen@pld-linux.org>.
+ */
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+struct stat st;
+int i, rv = 0;
+
+	if (argc == 1) {
+		fprintf(stderr, "This program is intended to be used by glibc postinstall stage.\n");
+		exit(1);
+	}
+
+	for (i = 1; i < argc; i++) {
+		char *path = argv[i];
+
+		if (stat(path, &st) == -1) {
+			// doesn't exist. good. next please.
+			if (errno == ENOENT) {
+				continue;
+			}
+			perror("stat");
+			exit(1);
+		}
+
+		if (S_ISDIR(st.st_mode)) {
+			char p[strlen(path) + sizeof(".rpmsave") + 1];
+
+			sprintf(p, "%s.rpmsave", path);
+			fprintf(stderr, "Renaming %s to %s\n", path, p);
+			if (rename(path, p) == -1) {
+				perror("rename");
+				rv = 1;
+			}
+		}
+	}
+
+	exit(rv);
+}
+
